42dcbc9d1836cf667ebd876237be6e7d18dbbd83
Leonardo Morabito <leonardomorabito02@gmail.com>
2022-05-26 18:46:45 -0300

    correccion de css, finalizacion de funcion js, arreglo de paginas

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,10 +1,47 @@
 function cargar_pagina() {
-    let opcion = document.getElementById("camaras").value;
+    const opcion = document.getElementById('camaras').value;
     switch (opcion) {
-        case "retrato":
-            window.open("../tipo_camara_1.html");
+        case "seleccion":
+            alert("Sleccione una opción");
             break;
-
+        case "retrato":
+            window.open('tipo_camara_1.html');
+            break;
+        case "publicitaria" :
+            window.open('tipo_camara_2.html');
+            break;
+        case "arquitectura":
+            window.open('tipo_camara_3.html');
+            break;
+        case "paisaje":
+            window.open('tipo_camara_4.html');
+            break;
+        case "deportes":
+            window.open('tipo_camara_5.html');
+            break;
+        case "macro":
+            window.open('tipo_camara_6.html');
+            break;
+        case "viajes":
+            window.open('tipo_camara_7.html');
+            break;
+        case "gastronomia":
+            window.open('tipo_camara_8.html');
+            break;
+        case "byn":
+            window.open('tipo_camara_9.html');
+            break;
     }
-
+}
+var canvas1 = document.getElementById("lienzo1");
+if (canvas1 && canvas1.getContext) {
+    var ctx1 = canvas1.getContext("2d");
+    if (ctx1) {
+        ctx1.lineWidth = 2;
+        ctx1.strokeStyle = "#00f";
+        ctx1.fillStyle = "#ff0";
+        ctx1.rect(60, 60, 150, 100);
+        ctx1.fill();
+        ctx1.stroke();
+    }
 }
